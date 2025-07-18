2bfb784508bde87e38f164ab7560e72363c15efc
nilayotu <nilayotu_07@hotmail.com>
2025-07-18 20:57:52 +0300

    challenge-1 has been completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 
-}
+        public static boolean checkForPalindrome(String text){
+
+            String cleanedStr = text.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+            LinkedList<Character> charList = new LinkedList<>();
+
+            for (char c : cleanedStr.toCharArray()) {
+                charList.add(c);
+            }
+
+            while (charList.size()>1){
+                if (!charList.pollFirst().equals(charList.pollLast())){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+    
